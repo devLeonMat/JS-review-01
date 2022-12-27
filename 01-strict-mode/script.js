65019b7de4c69c8df9d6c99b458cb877d0414af3
Leon Matias <roslin.pineda@thebridge.social>
2022-12-27 17:14:55 -0500

    save new examples

---
+++ 01-strict-mode/script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+
+if(hasDriversLicense) console.log('I can drive :D');
+
+// not allowed
+// const private = '';
+// const interface = '';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
